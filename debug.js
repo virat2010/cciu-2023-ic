992e7b471c97facb083a73bf0e5187a838078b3a
virat2010 <59180983+virat2010@users.noreply.github.com>
2023-04-04 21:56:42 -0400

    asd sa

---
+++ debug.js
@@ -0,0 +1,1 @@
+setInterval(fetch("http://localhost:8080").then(response => response.json()).then(jsonData => console.log(jsonData)),100)
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
-import { Suspense } from "react";
-
+import { Suspense } from "react"
 export default function Home() {
   return (
     <div>
@@ -13,7 +12,8 @@ export default function Home() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="#be185d" viewBox="0 0 24 24" className="inline-block animate-ping w-16 h-16 stroke-pink-700"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
               <svg xmlns="http://www.w3.org/2000/svg" fill="#be185d" viewBox="0 0 24 24" className="inline-block ml-[-4rem] w-16 h-16 stroke-pink-700"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
             </div>
-            <div className="stat-desc">7% less than last month</div>
+            <div className="stat-desc">7% less than last month
+            </div>
             <div className="radial-progress bg-base-200 border-primary text-primary-content font-extrabold text-4xl stat-figure animate-[spin_2s_cubic-bezier(.76,.01,.44,.95)]" style={{ "--value": 5, "--thickness": "8px", "--size": "12rem" }}>
               <div className="animate-[spin_2s_cubic-bezier(.86,.39,.11,.82)]" style={{ 'animation-direction': 'reverse' }}>5%</div>
             </div>
